017b9113a3d8f20828b15ec69e207db4f32cf47f
Tony_Huang <Tony_Huang@TONY_HUANG666>
2023-08-04 11:23:38 +0800

    成功

---
+++ User/main.c
@@ -0,0 +1,25 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+#include "PWM.h"
+
+uint8_t i;
+
+int main(void)
+{
+	OLED_Init();
+	PWM_Init();
+	while (1)
+	{
+		for(i = 0; i < 100; i++)
+		{
+			PWM_SetDuty(i);
+			Delay_ms(10);
+		}
+		for(i = 0; i < 100; i++)
+		{
+			PWM_SetDuty(100-i);
+			Delay_ms(10);
+		}
+	}
+}
